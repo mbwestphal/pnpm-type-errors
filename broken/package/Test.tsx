b7a805d9d57e88595e4980dd2093f607ffd9c8b0
Benjamin Westphal <benjamin.westphal@macmon.eu>
2024-11-11 17:03:19 +0100

    add working case

---
+++ broken/package/Test.tsx
@@ -0,0 +1,7 @@
+import {DateTimePicker} from '@mui/x-date-pickers-pro';
+import type {FC} from 'react';
+import React from 'react';
+
+const Test: FC = () => <DateTimePicker slotProps={{textField: {size: 'error-1'}}}/>;
+
+export default Test;
