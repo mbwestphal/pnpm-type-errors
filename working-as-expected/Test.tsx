a047de51480be5192ff2b44175ca34ba7ad324ba
Benjamin Westphal <benjamin.westphal@macmon.eu>
2024-11-11 17:06:04 +0100

    wording

--- working-as-expected/Test.tsx
+++ working-as-expected/Test.tsx
@@ -2,6 +2,6 @@ import {DateTimePicker} from '@mui/x-date-pickers-pro';
 import type {FC} from 'react';
 import React from 'react';
 
-const Test: FC = () => <DateTimePicker slotProps={{textField: {size: 'error-1'}}} onFuck={NaN}/>;
+const Test: FC = () => <DateTimePicker slotProps={{textField: {size: 'error-1'}}} />;
 
 export default Test;
